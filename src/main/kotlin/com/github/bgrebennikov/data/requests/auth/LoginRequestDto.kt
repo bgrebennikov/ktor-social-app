3d7ba54521d147371a017a217d141f1bfc180fc3
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-04 21:56:24 +0400

    make serializable

--- src/main/kotlin/com/github/bgrebennikov/data/requests/auth/LoginRequestDto.kt
+++ src/main/kotlin/com/github/bgrebennikov/data/requests/auth/LoginRequestDto.kt
@@ -1,5 +1,8 @@
 package com.github.bgrebennikov.data.requests.auth
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class LoginRequestDto(
     val login: String,
     val password: String
