661210d511a05aa7bc7caa8d750ed56685d8dc95
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-04 21:24:12 +0400

    initial commit

---
+++ src/main/kotlin/com/github/bgrebennikov/Application.kt
@@ -0,0 +1,16 @@
+package com.github.bgrebennikov
+
+import io.ktor.server.application.*
+import com.github.bgrebennikov.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureHTTP()
+    configureSecurity()
+    configureMonitoring()
+    configureRouting()
+}
---
+++ src/test/kotlin/com/github/bgrebennikov/ApplicationTest.kt
@@ -0,0 +1,39 @@
+package com.github.bgrebennikov
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.serialization.gson.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.auth.*
+import io.ktor.util.*
+import io.ktor.server.auth.jwt.*
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.server.resources.Resources
+import io.ktor.resources.*
+import kotlinx.serialization.Serializable
+import io.ktor.server.plugins.callloging.*
+import org.slf4j.event.*
+import io.ktor.server.request.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import com.github.bgrebennikov.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
