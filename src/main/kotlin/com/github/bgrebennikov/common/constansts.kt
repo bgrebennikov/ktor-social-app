f9eb4b5941d4f6ed8e2d10b991525da3cade3da1
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-04 21:40:26 +0400

    Global constants added

---
+++ src/main/kotlin/com/github/bgrebennikov/common/constansts.kt
@@ -0,0 +1,6 @@
+package com.github.bgrebennikov.common
+
+const val JWT_AUDIENCE = "jwt.audience"
+const val JWT_REALM = "jwt.realm"
+const val JWT_SECRET = "edrica_azaza"
+const val JWT_DOMAIN = "jwt.domain"
