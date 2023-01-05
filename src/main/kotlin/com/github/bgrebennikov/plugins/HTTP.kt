3b5fde9d2a0999725831e528dd1cab035cd76d10
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-05 21:21:56 +0400

    allow post method

--- src/main/kotlin/com/github/bgrebennikov/plugins/HTTP.kt
+++ src/main/kotlin/com/github/bgrebennikov/plugins/HTTP.kt
@@ -12,9 +12,9 @@ fun Application.configureHTTP() {
         allowMethod(HttpMethod.Put)
         allowMethod(HttpMethod.Delete)
         allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Post)
         allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        anyHost()
     }
 
 }
