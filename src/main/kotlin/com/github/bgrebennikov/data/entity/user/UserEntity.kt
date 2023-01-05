01b1043e43b49b382c5e15072f1951a01c97a1c2
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-05 22:33:39 +0400

    init user entity

---
+++ src/main/kotlin/com/github/bgrebennikov/data/entity/user/UserEntity.kt
@@ -0,0 +1,20 @@
+package com.github.bgrebennikov.data.entity.user
+
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+
+open class UserId (open val id : String = ObjectId().toString())
+
+data class UserEntity(
+    @BsonId
+    override val id: String,
+    val token: String,
+    val passwordHash: String,
+    val profile: Test
+) : UserId() {
+
+
+    data class Test(
+        override val id: String
+    ) : UserId()
+}
