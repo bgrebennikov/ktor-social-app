a5b47c8e2cd1eef1dd2fa6690432b96f2b060e56
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-05 21:41:45 +0400

    remove useless code

--- src/main/kotlin/com/github/bgrebennikov/DI/modules/DatabaseModule.kt
+++ src/main/kotlin/com/github/bgrebennikov/DI/modules/DatabaseModule.kt
@@ -1,8 +1,12 @@
 package com.github.bgrebennikov.DI.modules
 
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
+
+
+const val DATABASE_INSTANCE_NAME = "dev"
 
 val databaseModule = module {
     single {
