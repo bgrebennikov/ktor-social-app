fbac5f345c5060a5d0c7869111cd5704a2369828
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-05 23:08:44 +0400

    remove useless features, rename classes

--- src/main/kotlin/com/github/bgrebennikov/data/entity/user/UserEntity.kt
+++ src/main/kotlin/com/github/bgrebennikov/data/entity/user/UserEntity.kt
@@ -3,19 +3,18 @@ package com.github.bgrebennikov.data.entity.user
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
 
-open class UserId (open val id : String = ObjectId().toString())
-
 data class UserEntity(
     @BsonId
-    override val id: String,
+    val id: String,
     val token: String,
     val passwordHash: String,
-    val profile: Test
-) : UserId() {
+    val profile: UserProfile
+) {
 
 
-    data class Test(
-        override val id: String,
+    data class UserProfile(
+        @BsonId
+        val id: String? = ObjectId().toString(),
         val firstName: String?
-    ) : UserId()
+    )
 }
