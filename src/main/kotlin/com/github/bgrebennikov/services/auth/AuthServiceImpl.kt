4ad4026db4d08b02d8a86c6d18f7097a0a691f41
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-05 23:13:08 +0400

    demo user registration for a first time

--- src/main/kotlin/com/github/bgrebennikov/services/auth/AuthServiceImpl.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/auth/AuthServiceImpl.kt
@@ -2,9 +2,30 @@ package com.github.bgrebennikov.services.auth
 
 import com.github.bgrebennikov.data.entity.user.UserEntity
 import com.github.bgrebennikov.data.requests.auth.SignupRequestDto
+import com.github.bgrebennikov.datasource.UserDataSource
+import org.bson.types.ObjectId
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class AuthServiceImpl : AuthService {
+class AuthServiceImpl : AuthService, KoinComponent {
+
+    private val userDataSource by inject<UserDataSource>()
+
     override suspend fun signUp(signupRequest: SignupRequestDto): UserEntity {
-        TODO("Not yet implemented")
+
+        val generatedId = ObjectId().toString()
+
+        return userDataSource.insertUser(
+            UserEntity(
+                id = generatedId,
+                token = "fsefsefsef",
+                passwordHash = signupRequest.password,
+                profile = UserEntity.UserProfile(
+                    id = generatedId,
+                    firstName = signupRequest.firstName
+                )
+            )
+        )
+
     }
 }
