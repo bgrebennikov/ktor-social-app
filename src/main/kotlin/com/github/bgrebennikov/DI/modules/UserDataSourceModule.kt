982b1e99e1c24fee2db43efbae5c90940274c6f6
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-05 22:46:33 +0400

    add User DataSource Module

---
+++ src/main/kotlin/com/github/bgrebennikov/DI/modules/UserDataSourceModule.kt
@@ -0,0 +1,11 @@
+package com.github.bgrebennikov.DI.modules
+
+import com.github.bgrebennikov.datasource.UserDataSource
+import com.github.bgrebennikov.datasource.UserDataSourceImpl
+import org.koin.dsl.module
+
+val userDataSourceModule = module {
+    single<UserDataSource> {
+        UserDataSourceImpl()
+    }
+} 
