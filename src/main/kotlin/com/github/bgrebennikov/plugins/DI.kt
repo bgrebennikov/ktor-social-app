ba260e9e244cde217c43ab7542b3a57cff07a859
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-05 21:43:41 +0400

    add DB module

--- src/main/kotlin/com/github/bgrebennikov/plugins/DI.kt
+++ src/main/kotlin/com/github/bgrebennikov/plugins/DI.kt
@@ -1,11 +1,14 @@
 package com.github.bgrebennikov.plugins
 
+import com.github.bgrebennikov.DI.modules.databaseModule
 import io.ktor.server.application.*
 import org.koin.core.context.startKoin
 
 
 fun Application.configureDI(){
     startKoin {
-
+        modules(
+            databaseModule
+        )
     }
 }
