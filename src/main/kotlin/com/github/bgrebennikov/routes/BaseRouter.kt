c59a0753d700929a7dd873ea69c2d86e4faf12ed
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-05 21:20:48 +0400

    add home and auth routes

--- src/main/kotlin/com/github/bgrebennikov/routes/BaseRouter.kt
+++ src/main/kotlin/com/github/bgrebennikov/routes/BaseRouter.kt
@@ -1,8 +1,10 @@
 package com.github.bgrebennikov.plugins
 
+import com.github.bgrebennikov.routes.auth.authRoutes
+import com.github.bgrebennikov.routes.homeRoute
 import io.ktor.server.routing.*
 
 fun Route.baseRouter(){
-
+    homeRoute()
+    authRoutes()
 }
-
