8ed47275f694fa6ad14cd6800bd14b8caa32e6c1
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-12 17:49:47 +0400

    call auth service for signup

--- src/main/kotlin/com/github/bgrebennikov/usecases/auth/SignupUseCase.kt
+++ src/main/kotlin/com/github/bgrebennikov/usecases/auth/SignupUseCase.kt
@@ -3,13 +3,15 @@ package com.github.bgrebennikov.usecases.auth
 import com.github.bgrebennikov.data.base.BaseResponse
 import com.github.bgrebennikov.data.entity.user.UserEntity
 import com.github.bgrebennikov.data.requests.auth.SignupRequestDto
+import com.github.bgrebennikov.services.auth.AuthService
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class SignupUseCase {
+class SignupUseCase : KoinComponent{
 
+    private val authService by inject<AuthService>()
     suspend operator fun invoke(request: SignupRequestDto) : BaseResponse<UserEntity>{
-
-        TODO()
-
+        return authService.signUp(request)
     }
 
 }
