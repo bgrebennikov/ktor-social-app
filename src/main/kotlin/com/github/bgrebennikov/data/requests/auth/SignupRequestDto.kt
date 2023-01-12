8daead8b1ac3fd48ced2b950efcca56be7733c84
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-12 17:53:47 +0400

    uncommented email field

--- src/main/kotlin/com/github/bgrebennikov/data/requests/auth/SignupRequestDto.kt
+++ src/main/kotlin/com/github/bgrebennikov/data/requests/auth/SignupRequestDto.kt
@@ -2,7 +2,7 @@ package com.github.bgrebennikov.data.requests.auth
 
 data class SignupRequestDto(
 //    val username: String,
-//    val email: String,
+    val email: String,
     val password: String,
     val firstName: String,
 //    val lastName: String
