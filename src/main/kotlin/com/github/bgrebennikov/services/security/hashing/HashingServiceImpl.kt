14090eadd6a21c9065f27970aed63f07bcb74a84
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-12 18:55:17 +0400

    complete password hash functions

--- src/main/kotlin/com/github/bgrebennikov/services/security/hashing/HashingServiceImpl.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/security/hashing/HashingServiceImpl.kt
@@ -2,6 +2,8 @@ package com.github.bgrebennikov.services.security.hashing
 
 import com.github.bgrebennikov.common.HASHING_SALT_PROPERTY
 import io.ktor.server.application.*
+import org.apache.commons.codec.binary.Hex
+import org.apache.commons.codec.digest.DigestUtils
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -11,10 +13,11 @@ class HashingServiceImpl : HashingService, KoinComponent {
     private val salt: String = application.environment.config.property(HASHING_SALT_PROPERTY).getString()
 
     override fun generateSaltedHash(value: String): String {
-        TODO("Not yet implemented")
+        val saltHex = Hex.encodeHexString(salt.toByteArray())
+        return DigestUtils.sha256Hex("$saltHex$value")
     }
 
     override fun verify(password: String, passwordHash: String): Boolean {
-        TODO("Not yet implemented")
+        return generateSaltedHash(password) == passwordHash
     }
 }
