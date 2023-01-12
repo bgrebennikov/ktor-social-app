c99fe9db9b56ede90589b33bf455200d31adaf80
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-12 18:51:26 +0400

    declare variables

--- src/main/kotlin/com/github/bgrebennikov/services/security/hashing/HashingServiceImpl.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/security/hashing/HashingServiceImpl.kt
@@ -1,6 +1,15 @@
 package com.github.bgrebennikov.services.security.hashing
 
-class HashingServiceImpl : HashingService {
+import com.github.bgrebennikov.common.HASHING_SALT_PROPERTY
+import io.ktor.server.application.*
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class HashingServiceImpl : HashingService, KoinComponent {
+
+    private val application by inject<Application>()
+    private val salt: String = application.environment.config.property(HASHING_SALT_PROPERTY).getString()
+
     override fun generateSaltedHash(value: String): String {
         TODO("Not yet implemented")
     }
