29d058d640409944620ddee427064af86be630cf
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-12 17:59:33 +0400

    add error descriptions for responses

---
+++ src/main/kotlin/com/github/bgrebennikov/data/base/ResponseError.kt
@@ -0,0 +1,15 @@
+package com.github.bgrebennikov.data.base
+
+data class ResponseError(
+    val message: String,
+    val field: String? = null,
+    val type: ErrorType
+)
+
+enum class ErrorType{
+    INCORRECT_EMAIL, MISSING_EMAIL, EMAIL_ALREADY_EXISTS,
+    INCORRECT_PASSWORD, INVALID_CREDENTIALS,
+    INVALID_INPUT_LENGTH,
+    TOKEN_EXPIRED,
+    INVALID_AUTHOR_ID
+}
