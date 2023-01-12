1e7727f8a7b62c1942cbcaab2c95e86139c46746
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-12 18:18:20 +0400

    declare JWT variables

--- src/main/kotlin/com/github/bgrebennikov/services/auth/jwt/JwtServiceImpl.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/auth/jwt/JwtServiceImpl.kt
@@ -1,10 +1,21 @@
 package com.github.bgrebennikov.services.auth.jwt
 
+import com.github.bgrebennikov.common.JWT_AUDIENCE
+import com.github.bgrebennikov.common.JWT_DOMAIN
 import com.github.bgrebennikov.data.requests.auth.SignupRequestDto
+import io.ktor.server.application.*
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import java.util.Date
 
-class JwtServiceImpl : JwtService {
+class JwtServiceImpl : JwtService, KoinComponent {
 
+    private val application by inject<Application>()
 
+    private val maxHours = 3_600_000 * 24
+    private val expiresAt = Date(System.currentTimeMillis() + maxHours)
+    private val jwtAudience = application.environment.config.property(JWT_AUDIENCE).getString()
+    private val issuer = application.environment.config.property(JWT_DOMAIN).getString()
 
     override suspend fun generateToken(request: SignupRequestDto, userId: String): String {
         TODO()
