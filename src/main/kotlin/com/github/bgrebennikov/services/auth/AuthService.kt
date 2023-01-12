1527d9280fbc1a19c3c453d0b1008cc18cce515c
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-12 17:50:20 +0400

    use base response in return function

--- src/main/kotlin/com/github/bgrebennikov/services/auth/AuthService.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/auth/AuthService.kt
@@ -1,10 +1,11 @@
 package com.github.bgrebennikov.services.auth
 
+import com.github.bgrebennikov.data.base.BaseResponse
 import com.github.bgrebennikov.data.entity.user.UserEntity
 import com.github.bgrebennikov.data.requests.auth.SignupRequestDto
 
 interface AuthService {
 
-    suspend fun signUp(signupRequest: SignupRequestDto) : UserEntity
+    suspend fun signUp(signupRequest: SignupRequestDto) : BaseResponse<UserEntity>
 
 }
