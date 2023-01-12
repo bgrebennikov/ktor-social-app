94f5e526fe73f3ab8a2ee527f3f957769deea8f1
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-12 18:23:00 +0400

    build token function added

--- src/main/kotlin/com/github/bgrebennikov/services/auth/jwt/JwtServiceImpl.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/auth/jwt/JwtServiceImpl.kt
@@ -1,8 +1,13 @@
 package com.github.bgrebennikov.services.auth.jwt
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.github.bgrebennikov.common.CLAIM_EMAIL
+import com.github.bgrebennikov.common.CLAIM_USER_ID
 import com.github.bgrebennikov.common.JWT_AUDIENCE
 import com.github.bgrebennikov.common.JWT_DOMAIN
 import com.github.bgrebennikov.data.requests.auth.SignupRequestDto
+import com.github.bgrebennikov.data.requests.auth.UserJwtPrincipal
 import io.ktor.server.application.*
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -16,6 +21,17 @@ class JwtServiceImpl : JwtService, KoinComponent {
     private val expiresAt = Date(System.currentTimeMillis() + maxHours)
     private val jwtAudience = application.environment.config.property(JWT_AUDIENCE).getString()
     private val issuer = application.environment.config.property(JWT_DOMAIN).getString()
+
+
+    private suspend fun buildJwtToken(claim: UserJwtPrincipal) : String{
+        return JWT.create()
+            .withAudience(jwtAudience)
+            .withIssuer(issuer)
+            .withExpiresAt(expiresAt)
+            .withClaim(CLAIM_USER_ID, claim.userId)
+            .withClaim(CLAIM_EMAIL, claim.email)
+            .sign(Algorithm.HMAC256("secret"))
+    }
 
     override suspend fun generateToken(request: SignupRequestDto, userId: String): String {
         TODO()
