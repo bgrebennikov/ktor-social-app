9954be13971472e2b39adda4f391376ddc72eeb2
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-12 18:25:47 +0400

    wrap Jwt service as DI module

--- src/main/kotlin/com/github/bgrebennikov/DI/modules/AuthServiceModule.kt
+++ src/main/kotlin/com/github/bgrebennikov/DI/modules/AuthServiceModule.kt
@@ -2,11 +2,17 @@ package com.github.bgrebennikov.DI.modules
 
 import com.github.bgrebennikov.services.auth.AuthService
 import com.github.bgrebennikov.services.auth.AuthServiceImpl
+import com.github.bgrebennikov.services.auth.jwt.JwtService
+import com.github.bgrebennikov.services.auth.jwt.JwtServiceImpl
 import org.koin.dsl.module
 
 
 val authServiceModule = module {
     single<AuthService> {
         AuthServiceImpl()
+    }
+
+    single<JwtService> {
+        JwtServiceImpl()
     }
 }
