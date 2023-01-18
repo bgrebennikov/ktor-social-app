006711c8418617384990a21788e10024047a6acb
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-18 23:08:45 +0400

    add user service

--- src/main/kotlin/com/github/bgrebennikov/DI/modules/UserDataSourceModule.kt
+++ src/main/kotlin/com/github/bgrebennikov/DI/modules/UserDataSourceModule.kt
@@ -2,10 +2,16 @@ package com.github.bgrebennikov.DI.modules
 
 import com.github.bgrebennikov.datasource.UserDataSource
 import com.github.bgrebennikov.datasource.UserDataSourceImpl
+import com.github.bgrebennikov.services.user.UserService
+import com.github.bgrebennikov.services.user.UserServiceImpl
 import org.koin.dsl.module
 
-val userDataSourceModule = module {
+val userModule = module {
     single<UserDataSource> {
         UserDataSourceImpl()
+    }
+
+    single<UserService>{
+        UserServiceImpl()
     }
 }
