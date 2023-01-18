e59f6b13e7fd5ae7af483fe07e1fa7b65301ff1f
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-18 22:47:34 +0400

    move jwt sign algorithm to separate variable

--- src/main/kotlin/com/github/bgrebennikov/common/constants.kt
+++ src/main/kotlin/com/github/bgrebennikov/common/constants.kt
@@ -1,5 +1,7 @@
 package com.github.bgrebennikov.common
 
+import com.auth0.jwt.algorithms.Algorithm
+
 const val JWT_AUDIENCE = "jwt.audience"
 const val JWT_REALM = "jwt.realm"
 const val JWT_SECRET = "edrica_azaza"
@@ -13,3 +15,5 @@ const val CLAIM_USER_ID = "userId"
 const val CLAIM_EMAIL = "email"
 
 const val HASHING_SALT_PROPERTY = "hashing.salt"
+
+val JWT_ALGORITHM: Algorithm = Algorithm.HMAC256(JWT_SECRET)
