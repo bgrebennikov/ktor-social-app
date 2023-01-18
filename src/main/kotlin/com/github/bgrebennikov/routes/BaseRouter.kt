a3f4ec1a829cacc84e41394bc6be29fd7f2f18b1
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-18 23:06:39 +0400

    add private routes (authorized access only)

--- src/main/kotlin/com/github/bgrebennikov/routes/BaseRouter.kt
+++ src/main/kotlin/com/github/bgrebennikov/routes/BaseRouter.kt
@@ -2,9 +2,15 @@ package com.github.bgrebennikov.plugins
 
 import com.github.bgrebennikov.routes.auth.authRoutes
 import com.github.bgrebennikov.routes.homeRoute
+import com.github.bgrebennikov.routes.user.userRoutes
+import io.ktor.server.auth.*
 import io.ktor.server.routing.*
 
 fun Route.baseRouter(){
     homeRoute()
     authRoutes()
+
+    authenticate {
+        userRoutes()
+    }
 }
