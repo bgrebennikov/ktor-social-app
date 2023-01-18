643fea5003a7341942cb766b7c66afd1c8aedb0d
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-18 22:48:07 +0400

    change secret key for a first time

--- src/main/kotlin/com/github/bgrebennikov/common/constants.kt
+++ src/main/kotlin/com/github/bgrebennikov/common/constants.kt
@@ -4,7 +4,7 @@ import com.auth0.jwt.algorithms.Algorithm
 
 const val JWT_AUDIENCE = "jwt.audience"
 const val JWT_REALM = "jwt.realm"
-const val JWT_SECRET = "edrica_azaza"
+const val JWT_SECRET = "secret"
 const val JWT_DOMAIN = "jwt.domain"
 
 const val FIELD_USER_ID = "userId"
