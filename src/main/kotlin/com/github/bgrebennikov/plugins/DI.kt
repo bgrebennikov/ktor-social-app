ba3b61c5b797096ae83ff55f48e16abcb974b0c7
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-18 23:06:07 +0400

    add user service

--- src/main/kotlin/com/github/bgrebennikov/plugins/DI.kt
+++ src/main/kotlin/com/github/bgrebennikov/plugins/DI.kt
@@ -2,7 +2,7 @@ package com.github.bgrebennikov.plugins
 
 import com.github.bgrebennikov.DI.modules.authServiceModule
 import com.github.bgrebennikov.DI.modules.databaseModule
-import com.github.bgrebennikov.DI.modules.userDataSourceModule
+import com.github.bgrebennikov.DI.modules.userModule
 import io.ktor.server.application.*
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
@@ -12,7 +12,7 @@ fun Application.configureDI(){
     startKoin {
         modules(
             databaseModule,
-            userDataSourceModule,
+            userModule,
             authServiceModule,
 
             module {
