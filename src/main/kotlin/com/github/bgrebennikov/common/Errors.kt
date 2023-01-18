c1ecd47346abbf29f67431b12f8a8179b8b3ff7d
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-18 19:13:28 +0400

    add Error descriptions for user and auth requests

---
+++ src/main/kotlin/com/github/bgrebennikov/common/Errors.kt
@@ -0,0 +1,35 @@
+package com.github.bgrebennikov.common
+
+import com.github.bgrebennikov.data.base.BaseResponse
+import com.github.bgrebennikov.data.base.ErrorType
+import com.github.bgrebennikov.data.base.ResponseError
+import com.github.bgrebennikov.data.entity.user.UserEntity
+import io.ktor.http.*
+
+object Errors {
+    object User {
+        val USER_ALREADY_EXISTS = BaseResponse<UserEntity>(
+            status = HttpStatusCode.BadRequest,
+            errors = listOf(
+                ResponseError(
+                    message = "User already exists",
+                    field = "email",
+                    type = ErrorType.EMAIL_ALREADY_EXISTS
+                )
+            )
+        )
+    }
+
+    object Auth {
+        val LOGIN_WRONG_CREDENTIALS = BaseResponse<UserEntity>(
+            status = HttpStatusCode.BadRequest,
+            errors = listOf(
+                ResponseError(
+                    message = "Wrong credentials. Check your login or password",
+                    type = ErrorType.WRONG_CREDENTIALS
+                )
+            )
+        )
+    }
+
+}
