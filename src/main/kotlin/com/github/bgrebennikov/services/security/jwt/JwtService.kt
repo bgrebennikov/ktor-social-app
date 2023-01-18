dad3866c128c363bd21e15ea4e30a6c50e3037c4
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-18 19:11:24 +0400

    add: generate token for login request

--- src/main/kotlin/com/github/bgrebennikov/services/security/jwt/JwtService.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/security/jwt/JwtService.kt
@@ -1,9 +1,11 @@
 package com.github.bgrebennikov.services.security.jwt
 
+import com.github.bgrebennikov.data.requests.auth.LoginRequestDto
 import com.github.bgrebennikov.data.requests.auth.SignupRequestDto
 
 interface JwtService {
 
     suspend fun generateToken(request: SignupRequestDto, userId: String) : String
+    suspend fun generateToken(request: LoginRequestDto, userId: String) : String
 
 }
