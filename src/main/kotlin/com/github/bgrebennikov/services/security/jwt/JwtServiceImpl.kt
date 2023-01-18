4691301858150a9ced4c4c114c8a2be33c7f0cb0
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-18 22:42:09 +0400

    fix: secret key variable for sign

--- src/main/kotlin/com/github/bgrebennikov/services/security/jwt/JwtServiceImpl.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/security/jwt/JwtServiceImpl.kt
@@ -2,10 +2,7 @@ package com.github.bgrebennikov.services.security.jwt
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import com.github.bgrebennikov.common.CLAIM_EMAIL
-import com.github.bgrebennikov.common.CLAIM_USER_ID
-import com.github.bgrebennikov.common.JWT_AUDIENCE
-import com.github.bgrebennikov.common.JWT_DOMAIN
+import com.github.bgrebennikov.common.*
 import com.github.bgrebennikov.data.requests.auth.LoginRequestDto
 import com.github.bgrebennikov.data.requests.auth.SignupRequestDto
 import com.github.bgrebennikov.data.requests.auth.UserJwtPrincipal
@@ -31,7 +28,7 @@ class JwtServiceImpl : JwtService, KoinComponent {
             .withExpiresAt(expiresAt)
             .withClaim(CLAIM_USER_ID, claim.userId)
             .withClaim(CLAIM_EMAIL, claim.email)
-            .sign(Algorithm.HMAC256("secret"))
+            .sign(Algorithm.HMAC256(JWT_SECRET))
     }
 
     override suspend fun generateToken(request: SignupRequestDto, userId: String): String {
