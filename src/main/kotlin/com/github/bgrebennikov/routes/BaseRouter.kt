617e8f1973c5cc96a81220e539abd6530315b3ed
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-25 14:24:56 +0400

    add upload routes, and make and make static files public

--- src/main/kotlin/com/github/bgrebennikov/routes/BaseRouter.kt
+++ src/main/kotlin/com/github/bgrebennikov/routes/BaseRouter.kt
@@ -1,16 +1,30 @@
 package com.github.bgrebennikov.plugins
 
+import com.github.bgrebennikov.common.UPLOADS_PATH_ROOT
 import com.github.bgrebennikov.routes.auth.authRoutes
 import com.github.bgrebennikov.routes.homeRoute
+import com.github.bgrebennikov.routes.upload.uploadRoutes
 import com.github.bgrebennikov.routes.user.userRoutes
 import io.ktor.server.auth.*
+import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Route.baseRouter(){
     homeRoute()
     authRoutes()
 
+    serveStaticFiles()
+
     authenticate {
         userRoutes()
+        uploadRoutes()
+    }
+}
+
+private fun Route.serveStaticFiles(){
+    static(UPLOADS_PATH_ROOT){
+        staticRootFolder = File(UPLOADS_PATH_ROOT)
+        files(".")
     }
 }
