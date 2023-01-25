4e6257240875f7b65f1a3408022aa94fbce6359f
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-25 14:15:06 +0400

    add function for set photo as avatar

--- src/main/kotlin/com/github/bgrebennikov/datasource/UserDataSource.kt
+++ src/main/kotlin/com/github/bgrebennikov/datasource/UserDataSource.kt
@@ -1,10 +1,13 @@
 package com.github.bgrebennikov.datasource
 
 import com.github.bgrebennikov.data.entity.user.UserEntity
+import com.github.bgrebennikov.data.uploads.PhotosEntity
 
 interface UserDataSource{
     suspend fun insertUser(user: UserEntity) : UserEntity
     suspend fun findUserByEmail(email : String) : UserEntity?
     suspend fun findUserById(userId: String) : UserEntity?
+
+    suspend fun updateUserAvatar(userId: String, source: PhotosEntity) : Boolean
 
 }
--- src/main/kotlin/com/github/bgrebennikov/datasource/UserDataSourceImpl.kt
+++ src/main/kotlin/com/github/bgrebennikov/datasource/UserDataSourceImpl.kt
@@ -1,9 +1,11 @@
 package com.github.bgrebennikov.datasource
 
 import com.github.bgrebennikov.data.entity.user.UserEntity
+import com.github.bgrebennikov.data.uploads.PhotosEntity
 import com.mongodb.client.model.Filters
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import org.litote.kmongo.*
 import org.litote.kmongo.coroutine.CoroutineDatabase
 
 class UserDataSourceImpl : UserDataSource, KoinComponent {
@@ -19,6 +21,15 @@ class UserDataSourceImpl : UserDataSource, KoinComponent {
 
     override suspend fun findUserById(userId: String): UserEntity? {
         return users.findOneById(userId)
+    }
+
+    override suspend fun updateUserAvatar(userId: String, source: PhotosEntity) : Boolean{
+        val upd = users.updateOne(
+            UserEntity::id eq userId,
+            setValue(UserEntity::profile / UserEntity.UserProfile::avatar, source.src)
+        )
+
+        return upd.wasAcknowledged()
     }
 
     override suspend fun insertUser(user: UserEntity): UserEntity {
