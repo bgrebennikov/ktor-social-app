32cb88919446cf349eb211d37b5471d735da61dc
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-25 14:22:14 +0400

    add uploadModule

--- src/main/kotlin/com/github/bgrebennikov/plugins/DI.kt
+++ src/main/kotlin/com/github/bgrebennikov/plugins/DI.kt
@@ -2,6 +2,7 @@ package com.github.bgrebennikov.plugins
 
 import com.github.bgrebennikov.DI.modules.authServiceModule
 import com.github.bgrebennikov.DI.modules.databaseModule
+import com.github.bgrebennikov.DI.modules.uploadModule
 import com.github.bgrebennikov.DI.modules.userModule
 import io.ktor.server.application.*
 import org.koin.core.context.startKoin
@@ -14,6 +15,7 @@ fun Application.configureDI(){
             databaseModule,
             userModule,
             authServiceModule,
+            uploadModule,
 
             module {
                 single {
