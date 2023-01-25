a448104c70989c1f3acb4ec4aa73819ec8d6c861
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-25 14:20:56 +0400

    add new error response templates

--- src/main/kotlin/com/github/bgrebennikov/common/Errors.kt
+++ src/main/kotlin/com/github/bgrebennikov/common/Errors.kt
@@ -4,6 +4,8 @@ import com.github.bgrebennikov.data.base.BaseResponse
 import com.github.bgrebennikov.data.base.ErrorType
 import com.github.bgrebennikov.data.base.ResponseError
 import com.github.bgrebennikov.data.entity.user.UserEntity
+import com.github.bgrebennikov.data.requests.user.edit.EditAvatarAction
+import com.github.bgrebennikov.data.requests.user.edit.EditAvatarResponse
 import io.ktor.http.*
 
 object Errors {
@@ -30,6 +32,18 @@ object Errors {
         )
     }
 
+    object Uploads {
+        val WRONG_UPLOAD_PHOTO_ID = BaseResponse(
+            response = EditAvatarResponse(action = EditAvatarAction.UPDATE),
+            errors = listOf(
+                ResponseError(
+                    message = "File upload id must be not empty",
+                    type = ErrorType.WRONG_UPLOAD_PHOTO_ID
+                )
+            )
+        )
+    }
+
     object Auth {
         val LOGIN_WRONG_CREDENTIALS = BaseResponse<UserEntity>(
             status = HttpStatusCode.BadRequest,
@@ -37,6 +51,20 @@ object Errors {
                 ResponseError(
                     message = "Wrong credentials. Check your login or password",
                     type = ErrorType.WRONG_CREDENTIALS
+                )
+            )
+        )
+    }
+
+    object Server{
+        val INTERNAL_ERROR = BaseResponse(
+            response = EditAvatarResponse(
+                EditAvatarAction.UPDATE
+            ),
+            errors = listOf(
+                ResponseError(
+                    message = "Something went wrong...",
+                    type = ErrorType.INTERNAL_SERVER_ERROR
                 )
             )
         )
