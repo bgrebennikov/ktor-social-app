56b75805532a7470667460d977d8fb06e8950755
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-26 13:33:20 +0400

    add token expired challenge

--- src/main/kotlin/com/github/bgrebennikov/plugins/Security.kt
+++ src/main/kotlin/com/github/bgrebennikov/plugins/Security.kt
@@ -1,16 +1,11 @@
 package com.github.bgrebennikov.plugins
 
-import io.ktor.server.auth.*
-import io.ktor.util.*
-import io.ktor.server.auth.jwt.*
 import com.auth0.jwt.JWT
-import com.auth0.jwt.JWTVerifier
-import com.auth0.jwt.algorithms.Algorithm
 import com.github.bgrebennikov.common.*
 import com.github.bgrebennikov.data.requests.auth.UserJwtPrincipal
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 
 fun Application.configureSecurity() {
 
@@ -27,13 +22,16 @@ fun Application.configureSecurity() {
             )
             validate { credential ->
                 if (credential.payload.audience.contains(jwtAudience)) {
-                    with(credential.payload){
+                    with(credential.payload) {
                         UserJwtPrincipal(
                             userId = getClaim(FIELD_USER_ID).asString(),
                             email = getClaim(FIELD_EMAIL).asString()
                         )
                     }
                 } else null
+            }
+            challenge { defaultScheme, realm ->
+                Errors.Auth.TOKEN_EXPIRED
             }
         }
     }
