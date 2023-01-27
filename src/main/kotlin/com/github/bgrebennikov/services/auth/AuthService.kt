569b6df8bc8677d57cd9d48cb933bb04aa20970d
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-27 22:42:15 +0400

    add logout and token blacklist check function

--- src/main/kotlin/com/github/bgrebennikov/services/auth/AuthService.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/auth/AuthService.kt
@@ -4,10 +4,15 @@ import com.github.bgrebennikov.data.auth.AuthResponse
 import com.github.bgrebennikov.data.base.BaseResponse
 import com.github.bgrebennikov.data.requests.auth.LoginRequestDto
 import com.github.bgrebennikov.data.requests.auth.SignupRequestDto
+import com.github.bgrebennikov.data.responses.auth.LogoutResponse
 
 interface AuthService {
 
     suspend fun signUp(signupRequest: SignupRequestDto) : BaseResponse<AuthResponse>
     suspend fun login(loginRequestDto: LoginRequestDto) : BaseResponse<AuthResponse>
+
+    suspend fun logout(userId: String, token: String) : BaseResponse<LogoutResponse>
+
+    suspend fun tokenIsDenny(token: String?) : Boolean
 
 }
--- src/main/kotlin/com/github/bgrebennikov/services/auth/AuthServiceImpl.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/auth/AuthServiceImpl.kt
@@ -1,19 +1,24 @@
 package com.github.bgrebennikov.services.auth
 
 import com.github.bgrebennikov.common.Errors
+import com.github.bgrebennikov.common.ONE_DAY
 import com.github.bgrebennikov.data.auth.AuthResponse
 import com.github.bgrebennikov.data.auth.SettingsEntity
 import com.github.bgrebennikov.data.base.BaseResponse
 import com.github.bgrebennikov.data.entity.user.UserEntity
 import com.github.bgrebennikov.data.requests.auth.LoginRequestDto
 import com.github.bgrebennikov.data.requests.auth.SignupRequestDto
+import com.github.bgrebennikov.data.requests.user.UserActions
+import com.github.bgrebennikov.data.responses.auth.LogoutResponse
 import com.github.bgrebennikov.datasource.AuthDataSource
+import com.github.bgrebennikov.datasource.RedisDataSource
 import com.github.bgrebennikov.datasource.UserDataSource
 import com.github.bgrebennikov.services.security.hashing.HashingService
 import com.github.bgrebennikov.services.security.jwt.JwtService
 import org.bson.types.ObjectId
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.util.concurrent.TimeUnit
 
 class AuthServiceImpl : AuthService, KoinComponent {
 
@@ -21,6 +26,8 @@ class AuthServiceImpl : AuthService, KoinComponent {
     private val authDataSource by inject<AuthDataSource>()
     private val hashingService by inject<HashingService>()
     private val jwtService by inject<JwtService>()
+
+    private val redisDataSource by inject<RedisDataSource>()
 
     private suspend fun userExists(email: String): Boolean {
         return userDataSource.findUserByEmail(email) != null
@@ -84,4 +91,21 @@ class AuthServiceImpl : AuthService, KoinComponent {
         )
 
     }
+
+    override suspend fun logout(userId: String, token: String): BaseResponse<LogoutResponse> {
+        val deny = redisDataSource.set(token, userId, TimeUnit.MILLISECONDS.toSeconds(ONE_DAY.toLong()))
+        return BaseResponse(
+            response = LogoutResponse(
+                UserActions.LOGOUT,
+                status = deny
+            )
+        )
+    }
+
+    override suspend fun tokenIsDenny(token: String?): Boolean {
+        token ?: return false
+        return redisDataSource.get(token) != null
+    }
+
+
 }
