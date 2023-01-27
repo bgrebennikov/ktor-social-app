d1b55aa1b427240249d9f52fb1a6114a3fafd320
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-27 16:14:57 +0400

    Change base response model to AuthResponse

--- src/main/kotlin/com/github/bgrebennikov/usecases/auth/SignupUseCase.kt
+++ src/main/kotlin/com/github/bgrebennikov/usecases/auth/SignupUseCase.kt
@@ -1,7 +1,7 @@
 package com.github.bgrebennikov.usecases.auth
 
+import com.github.bgrebennikov.data.auth.AuthResponse
 import com.github.bgrebennikov.data.base.BaseResponse
-import com.github.bgrebennikov.data.entity.user.UserEntity
 import com.github.bgrebennikov.data.requests.auth.SignupRequestDto
 import com.github.bgrebennikov.services.auth.AuthService
 import org.koin.core.component.KoinComponent
@@ -10,7 +10,7 @@ import org.koin.core.component.inject
 class SignupUseCase : KoinComponent{
 
     private val authService by inject<AuthService>()
-    suspend operator fun invoke(request: SignupRequestDto) : BaseResponse<UserEntity>{
+    suspend operator fun invoke(request: SignupRequestDto) : BaseResponse<AuthResponse>{
         return authService.signUp(request)
     }
 
