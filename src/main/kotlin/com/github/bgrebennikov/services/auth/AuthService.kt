ab1288ca483c714ab2521ce2b7c2552e51a9ad4c
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-27 16:26:32 +0400

    change base response body to AuthResponse

--- src/main/kotlin/com/github/bgrebennikov/services/auth/AuthService.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/auth/AuthService.kt
@@ -1,13 +1,13 @@
 package com.github.bgrebennikov.services.auth
 
+import com.github.bgrebennikov.data.auth.AuthResponse
 import com.github.bgrebennikov.data.base.BaseResponse
-import com.github.bgrebennikov.data.entity.user.UserEntity
 import com.github.bgrebennikov.data.requests.auth.LoginRequestDto
 import com.github.bgrebennikov.data.requests.auth.SignupRequestDto
 
 interface AuthService {
 
-    suspend fun signUp(signupRequest: SignupRequestDto) : BaseResponse<UserEntity>
-    suspend fun login(loginRequestDto: LoginRequestDto) : BaseResponse<UserEntity>
+    suspend fun signUp(signupRequest: SignupRequestDto) : BaseResponse<AuthResponse>
+    suspend fun login(loginRequestDto: LoginRequestDto) : BaseResponse<AuthResponse>
 
 }
