96eb391e02404b672e590f4ce79ef86ed67e2622
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-27 22:48:20 +0400

    add response for unresolved user action

--- src/main/kotlin/com/github/bgrebennikov/usecases/user/edit/EditAvatarUseCase.kt
+++ src/main/kotlin/com/github/bgrebennikov/usecases/user/edit/EditAvatarUseCase.kt
@@ -1,11 +1,12 @@
 package com.github.bgrebennikov.usecases.user.edit
 
+import com.github.bgrebennikov.common.Errors
 import com.github.bgrebennikov.data.base.BaseResponse
 import com.github.bgrebennikov.data.base.ErrorType
 import com.github.bgrebennikov.data.base.ResponseError
-import com.github.bgrebennikov.data.requests.user.edit.EditAvatarAction
+import com.github.bgrebennikov.data.requests.user.UserActions
 import com.github.bgrebennikov.data.requests.user.edit.EditAvatarRequest
-import com.github.bgrebennikov.data.requests.user.edit.EditAvatarResponse
+import com.github.bgrebennikov.data.responses.user.edit.EditAvatarResponse
 import com.github.bgrebennikov.services.user.UserService
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -16,16 +17,18 @@ class EditAvatarUseCase : KoinComponent {
 
     suspend operator fun invoke(request: EditAvatarRequest, userId: String?): BaseResponse<EditAvatarResponse> {
         userId ?: return BaseResponse(
-            response = EditAvatarResponse(EditAvatarAction.UPDATE),
+            response = EditAvatarResponse(UserActions.UPDATE),
             errors = listOf(
                 ResponseError("Something went wrong...", type = ErrorType.INTERNAL_SERVER_ERROR)
             )
         )
         return when (request.action) {
-            EditAvatarAction.UPDATE -> userService.updateAvatar(userId, request.photoId)
-            EditAvatarAction.REMOVE -> BaseResponse(
-                response = EditAvatarResponse(EditAvatarAction.REMOVE)
+            UserActions.UPDATE -> userService.updateAvatar(userId, request.photoId)
+            UserActions.REMOVE -> BaseResponse(
+                response = EditAvatarResponse(UserActions.REMOVE)
             )
+
+            else -> Errors.User.UNRESOLVED_USER_ACTION
         }
     }
 
