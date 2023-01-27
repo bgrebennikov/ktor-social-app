c48fba04c57c85a120453fec18832b61383c1063
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-27 22:48:49 +0400

    upd: EditAvatarRequest

--- src/main/kotlin/com/github/bgrebennikov/data/requests/user/edit/EditAvatarResponse.kt
+++ src/main/kotlin/com/github/bgrebennikov/data/requests/user/edit/EditAvatarResponse.kt
@@ -1,6 +1,8 @@
 package com.github.bgrebennikov.data.requests.user.edit
 
-data class EditAvatarResponse(
-    val action: EditAvatarAction,
-    val source: String? = null,
+import com.github.bgrebennikov.data.requests.user.UserActions
+
+data class EditAvatarRequest(
+    val action: UserActions,
+    val photoId: String?
 )
