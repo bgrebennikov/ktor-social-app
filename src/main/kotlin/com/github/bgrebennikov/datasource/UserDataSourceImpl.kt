a917ed4faed16e2aa26580aca5adeb913d0be1b0
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-28 15:26:20 +0400

    update findUserById function with type safe variable

--- src/main/kotlin/com/github/bgrebennikov/datasource/UserDataSourceImpl.kt
+++ src/main/kotlin/com/github/bgrebennikov/datasource/UserDataSourceImpl.kt
@@ -2,11 +2,12 @@ package com.github.bgrebennikov.datasource
 
 import com.github.bgrebennikov.data.entity.user.UserEntity
 import com.github.bgrebennikov.data.uploads.PhotosEntity
-import com.mongodb.client.model.Filters
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import org.litote.kmongo.*
 import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.div
+import org.litote.kmongo.eq
+import org.litote.kmongo.push
 
 class UserDataSourceImpl : UserDataSource, KoinComponent {
 
@@ -15,7 +16,7 @@ class UserDataSourceImpl : UserDataSource, KoinComponent {
     private val users = database.getCollection<UserEntity>()
     override suspend fun findUserByEmail(email: String): UserEntity? {
         return users.findOne(
-            Filters.eq("profile.email", email)
+            UserEntity.UserProfile::email eq email
         )
     }
 
@@ -23,10 +24,16 @@ class UserDataSourceImpl : UserDataSource, KoinComponent {
         return users.findOneById(userId)
     }
 
-    override suspend fun updateUserAvatar(userId: String, source: PhotosEntity) : Boolean{
+    override suspend fun updateUserAvatar(userId: String, source: PhotosEntity): Boolean {
         val upd = users.updateOne(
             UserEntity::id eq userId,
-            setValue(UserEntity::profile / UserEntity.UserProfile::avatar, source.src)
+            push(
+                UserEntity::profile / UserEntity.UserProfile::avatar,
+                UserEntity.Avatar(
+                    source.src,
+                    source.thumbnails
+                )
+            )
         )
 
         return upd.wasAcknowledged()
