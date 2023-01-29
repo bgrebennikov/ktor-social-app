445e0b0e43dba06ff8a4100002aaace95b49fdef
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-29 22:23:56 +0400

    remove unused imports in configureRouting

--- src/main/kotlin/com/github/bgrebennikov/plugins/Routing.kt
+++ src/main/kotlin/com/github/bgrebennikov/plugins/Routing.kt
@@ -1,20 +1,13 @@
 package com.github.bgrebennikov.plugins
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.server.resources.Resources
-import io.ktor.resources.*
-import kotlinx.serialization.Serializable
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.resources.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     install(Resources)
 
     routing {
-
         baseRouter()
-
     }
 }
