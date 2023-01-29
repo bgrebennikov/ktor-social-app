5a3a479ecc97de238204c71ca72ede5c4311a9da
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-01-29 22:21:37 +0400

    remove status filed in LogoutResponse

--- src/main/kotlin/com/github/bgrebennikov/data/responses/auth/LogoutResponse.kt
+++ src/main/kotlin/com/github/bgrebennikov/data/responses/auth/LogoutResponse.kt
@@ -3,6 +3,5 @@ package com.github.bgrebennikov.data.responses.auth
 import com.github.bgrebennikov.data.requests.user.UserActions
 
 data class LogoutResponse(
-    val action: UserActions,
-    val status: String? = null
+    val action: UserActions
 )
