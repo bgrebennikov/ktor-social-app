44ce89c98906837bc1628d419b1626f297f755fe
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-05 13:44:31 +0400

    add editHobbies and editAbout routes

--- src/main/kotlin/com/github/bgrebennikov/routes/user/UserRoutes.kt
+++ src/main/kotlin/com/github/bgrebennikov/routes/user/UserRoutes.kt
@@ -1,9 +1,13 @@
 package com.github.bgrebennikov.routes.user
 
+import com.github.bgrebennikov.data.requests.user.edit.EditAboutRequest
 import com.github.bgrebennikov.data.requests.user.edit.EditAvatarRequest
+import com.github.bgrebennikov.data.requests.user.edit.EditHobbiesRequest
 import com.github.bgrebennikov.plugins.jwtUser
 import com.github.bgrebennikov.usecases.user.InitUserUseCase
+import com.github.bgrebennikov.usecases.user.edit.EditAboutUseCase
 import com.github.bgrebennikov.usecases.user.edit.EditAvatarUseCase
+import com.github.bgrebennikov.usecases.user.edit.EditHobbiesUseCase
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
@@ -17,6 +21,12 @@ fun Route.userRoutes(){
         route("edit"){
             route("avatar"){
                 editAvatar()
+            }
+            route("hobbies"){
+                editHobbies()
+            }
+            route("about"){
+                editAbout()
             }
         }
     }
@@ -33,5 +43,27 @@ private fun Route.editAvatar(){
 
         val request = call.receiveNullable<EditAvatarRequest>() ?: return@post
         call.respond(EditAvatarUseCase().invoke(request, call.jwtUser?.userId))
+    }
+}
+
+private fun Route.editHobbies(){
+    post {
+
+        val userId = call.jwtUser?.userId ?: return@post
+        val request = call.receiveNullable<EditHobbiesRequest>() ?: return@post
+
+        call.respond(
+            EditHobbiesUseCase().invoke(userId, request)
+        )
+    }
+}
+
+private fun Route.editAbout(){
+    post {
+        val userId = call.jwtUser?.userId ?: return@post
+        val request = call.receiveNullable<EditAboutRequest>() ?: return@post
+
+        call.respond(EditAboutUseCase().invoke(userId, request))
+
     }
 }
