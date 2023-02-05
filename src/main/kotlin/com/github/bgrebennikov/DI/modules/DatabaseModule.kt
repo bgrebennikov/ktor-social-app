346c9d18b208526b7d7dd0e2c7eb01ac4973c546
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-05 13:42:20 +0400

    change database connection params

--- src/main/kotlin/com/github/bgrebennikov/DI/modules/DatabaseModule.kt
+++ src/main/kotlin/com/github/bgrebennikov/DI/modules/DatabaseModule.kt
@@ -1,17 +1,15 @@
 package com.github.bgrebennikov.DI.modules
 
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
-
-const val DATABASE_INSTANCE_NAME = "dev"
-
 val databaseModule = module {
     single {
         KMongo
-            .createClient()
+            .createClient(
+                "mongodb+srv://ivUser:Icu25050032&$@cluster0.ueh7iy4.mongodb.net/test",
+            )
             .coroutine
             .getDatabase("SocialApp")
     }
