4bdc7f2e42986d0d4b00cf9297450c646237c2c9
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-05 13:35:13 +0400

    add decodeRefreshToken function

--- src/main/kotlin/com/github/bgrebennikov/services/security/jwt/JwtServiceImpl.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/security/jwt/JwtServiceImpl.kt
@@ -1,6 +1,8 @@
 package com.github.bgrebennikov.services.security.jwt
 
 import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.interfaces.DecodedJWT
 import com.github.bgrebennikov.common.*
 import com.github.bgrebennikov.data.jwt.UserJwtPrincipal
 import io.ktor.server.application.*
@@ -11,33 +13,55 @@ import java.util.*
 class JwtServiceImpl : JwtService, KoinComponent {
 
     private val application by inject<Application>()
-    private val jwtAudience = application.environment.config.property(JWT_AUDIENCE).getString()
-    private val issuer = application.environment.config.property(JWT_DOMAIN).getString()
+    private val jwtAudience = application.environment.config.property(JWT_ACCESS_TOKEN_AUDIENCE).getString()
+    private val issuer = application.environment.config.property(JWT_ISSUER).getString()
 
-    private fun buildAccessJwtToken(claim: UserJwtPrincipal, expiresAt: Date) : String{
+    private val refreshTokenVerifier: JWTVerifier =
+        JWT.require(JWT_REFRESH_ALGORITHM)
+            .withAudience(jwtAudience)
+            .withIssuer(issuer)
+            .build()
+
+    private fun buildAccessJwtToken(userJwt: UserJwtPrincipal): String {
         return JWT.create()
             .withAudience(jwtAudience)
             .withIssuer(issuer)
-            .withExpiresAt(expiresAt)
-            .withClaim(CLAIM_USER_ID, claim.userId)
-            .withClaim(CLAIM_EMAIL, claim.email)
+            .withExpiresAt(userJwt.expireAt)
+            .withClaim(CLAIM_USER_ID, userJwt.userId)
+            .withClaim(CLAIM_EMAIL, userJwt.email)
             .sign(JWT_ALGORITHM)
     }
 
-    private fun buildRefreshJwtToken(expiresAt: Date) : String{
+    private fun buildRefreshJwtToken(userJwt: UserJwtPrincipal): String {
         return JWT.create()
             .withAudience(jwtAudience)
             .withIssuer(issuer)
-            .withExpiresAt(expiresAt)
-            .sign(JWT_ALGORITHM)
+            .withClaim(CLAIM_USER_ID, userJwt.userId)
+            .withClaim(CLAIM_EMAIL, userJwt.email)
+            .withExpiresAt(userJwt.expireAt)
+            .sign(JWT_REFRESH_ALGORITHM)
+    }
+
+    override suspend fun decodeRefreshToken(refreshToken: String): DecodedJWT? {
+
+        val decodedJWT = refreshTokenVerifier.verify(refreshToken)
+
+        with(decodedJWT) {
+            val userId = getClaim(FIELD_USER_ID) ?: return null
+            val email = getClaim(FIELD_EMAIL) ?: return null
+        }
+
+        return decodedJWT
     }
 
     override suspend fun generateAccessToken(email: String, userId: String): String {
-        return buildAccessJwtToken(UserJwtPrincipal(userId, email), ACCESS_TOKEN_EXPIRE_DATE)
+        val expireAt = Date(System.currentTimeMillis() + ONE_DAY)
+        return buildAccessJwtToken(UserJwtPrincipal(userId, email, expireAt))
     }
 
 
-    override suspend fun generateRefreshToken(): String {
-        return buildRefreshJwtToken(REFRESH_TOKEN_EXPIRE_DATE)
+    override suspend fun generateRefreshToken(email: String, userId: String): String {
+        val expireAt = Date(System.currentTimeMillis() + ONE_MONTH)
+        return buildRefreshJwtToken(UserJwtPrincipal(userId, email, expireAt))
     }
 }
