a5fd24f09021c6982bb6478e133a30938ee440b3
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-05 13:39:28 +0400

    add expireIn field to UserJwtPrincipal

--- src/main/kotlin/com/github/bgrebennikov/data/jwt/UserJwtPrincipal.kt
+++ src/main/kotlin/com/github/bgrebennikov/data/jwt/UserJwtPrincipal.kt
@@ -1,8 +1,10 @@
 package com.github.bgrebennikov.data.jwt
 
 import io.ktor.server.auth.*
+import java.util.*
 
 data class UserJwtPrincipal(
     val userId: String,
-    val email: String
+    val email: String,
+    val expireAt: Date
 ) : Principal
