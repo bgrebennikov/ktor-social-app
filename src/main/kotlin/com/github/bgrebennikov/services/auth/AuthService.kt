443c8b04f20eabe9f4b4ee0a79ea8183d4ebb95d
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-05 13:43:21 +0400

    add functions for refreshing user access token

--- src/main/kotlin/com/github/bgrebennikov/services/auth/AuthService.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/auth/AuthService.kt
@@ -1,16 +1,16 @@
 package com.github.bgrebennikov.services.auth
 
-import com.github.bgrebennikov.data.auth.AuthResponse
 import com.github.bgrebennikov.data.base.BaseResponse
 import com.github.bgrebennikov.data.requests.auth.LoginRequest
 import com.github.bgrebennikov.data.requests.auth.SignupRequest
+import com.github.bgrebennikov.data.responses.auth.AuthResponse
 import com.github.bgrebennikov.data.responses.auth.LogoutResponse
 
 interface AuthService {
 
     suspend fun signUp(signupRequest: SignupRequest) : BaseResponse<AuthResponse>
     suspend fun login(loginRequest: LoginRequest) : BaseResponse<AuthResponse>
-
+    suspend fun refresh(refreshToken: String): BaseResponse<AuthResponse>
     suspend fun logout(userId: String, token: String) : BaseResponse<LogoutResponse>
 
     suspend fun tokenIsDenny(token: String?) : Boolean
