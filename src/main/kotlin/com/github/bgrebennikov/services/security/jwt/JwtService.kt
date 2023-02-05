06848bdf86735b56925d74d8072a5e1a02a212f0
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-05 13:34:59 +0400

    add decodeRefreshToken function

--- src/main/kotlin/com/github/bgrebennikov/services/security/jwt/JwtService.kt
+++ src/main/kotlin/com/github/bgrebennikov/services/security/jwt/JwtService.kt
@@ -1,8 +1,12 @@
 package com.github.bgrebennikov.services.security.jwt
 
+import com.auth0.jwt.interfaces.DecodedJWT
+
 interface JwtService {
 
     suspend fun generateAccessToken(email: String, userId: String) : String
-    suspend fun generateRefreshToken() : String
+    suspend fun generateRefreshToken(email: String, userId: String) : String
+
+    suspend fun decodeRefreshToken(refreshToken: String) : DecodedJWT?
 
 }
