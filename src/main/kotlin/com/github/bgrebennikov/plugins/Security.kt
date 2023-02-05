e3d05ca170bcd64228a233206fd16eaf3deff948
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-05 13:40:05 +0400

    move jwt config to separate class

--- src/main/kotlin/com/github/bgrebennikov/plugins/Security.kt
+++ src/main/kotlin/com/github/bgrebennikov/plugins/Security.kt
@@ -1,57 +1,34 @@
 package com.github.bgrebennikov.plugins
 
-import com.auth0.jwt.JWT
 import com.github.bgrebennikov.common.*
 import com.github.bgrebennikov.data.jwt.UserJwtPrincipal
-import com.github.bgrebennikov.services.auth.AuthService
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
-import org.koin.ktor.ext.inject
 
 fun Application.configureSecurity() {
 
-    val authService by inject<AuthService>()
+    val jwtConfig = JwtConfig( this)
+    val jwtRealm = this.environment.config.property(JWT_ACCESS_TOKEN_REALM).getString()
 
     authentication {
-        jwt {
-            val jwtAudience = this@configureSecurity.environment.config.property(JWT_AUDIENCE).getString()
-            realm = this@configureSecurity.environment.config.property(JWT_REALM).getString()
+        jwt(ACCESS_TOKEN) {
+            realm = jwtRealm
             verifier(
-                JWT
-                    .require(JWT_ALGORITHM)
-                    .withAudience(jwtAudience)
-                    .withIssuer(this@configureSecurity.environment.config.property(JWT_DOMAIN).getString())
-                    .build()
+                jwtConfig.accessTokenVerifier
             )
-            validate { credential ->
-
-                if (authService.tokenIsDenny(userToken)) return@validate null
-
-                if (credential.payload.audience.contains(jwtAudience)) {
-                    with(credential.payload) {
-
-                        val userId = getClaim(FIELD_USER_ID) ?: return@validate null
-                        val email = getClaim(FIELD_EMAIL) ?: return@validate null
-
-                        UserJwtPrincipal(
-                            userId = userId.asString(),
-                            email = email.asString()
-                        )
-                    }
-                } else null
+            validate { credentials ->
+                jwtConfig.validateCredentials(this, credentials)
             }
             challenge { _, _ ->
                 call.respond(HttpStatusCode.Unauthorized, Errors.Auth.TOKEN_EXPIRED)
             }
         }
     }
-
 }
-
 
 val ApplicationCall.jwtUser get() = principal<UserJwtPrincipal>()
 val ApplicationCall.userToken get() = request.authorization()?.split(" ")?.last()
